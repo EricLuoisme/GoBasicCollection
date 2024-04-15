1dd87d4ed3692d9f887c410c24d1b00e17d54457
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-04-15 23:42:59 +0800

    chore: by using LittleEden coding, server can recognize how many size of bytes need to be allocated when receiving a incoming bytes stream which construct a file

--- smallProject/fileServer/main.go
+++ smallProject/fileServer/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
@@ -31,15 +33,36 @@ func (f *FileServer) start() {
 	}
 }
 
+// readLoopFull 固定的size会导致Client传输的文件大小超出这个size时, Server会丢失部分内容
+//func (f *FileServer) readLoopFull(conn net.Conn) {
+//	buf := make([]byte, 2048)
+//	for {
+//		n, err := conn.Read(buf)
+//		if err != nil {
+//			log.Fatalln(err)
+//		}
+//		file := buf[:n]
+//		fmt.Println(file)
+//		fmt.Printf("received %d bytes over network\n", n)
+//	}
+//}
+
+// readLoop 使用buffer进行缓存
 func (f *FileServer) readLoop(conn net.Conn) {
-	buf := make([]byte, 2048)
+	buf := new(bytes.Buffer)
 	for {
-		n, err := conn.Read(buf)
+		// 使用小端编码的特性, 将大小提取出来
+		var size int64
+		binary.Read(conn, binary.LittleEndian, &size)
+
+		// 使用copy来将connection读取到的字节copy到buffer中
+		// 但需要注意, Copy会直到EOF, 如果没有send, 那么会一直阻塞
+		// 这里也需要copyN
+		n, err := io.CopyN(buf, conn, size)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		file := buf[:n]
-		fmt.Println(file)
+		fmt.Println(buf.Bytes())
 		fmt.Printf("received %d bytes over network\n", n)
 	}
 }
@@ -60,8 +83,14 @@ func sendFile(size int) error {
 		return err
 	}
 
-	// 写文件流给Server
-	n, err := conn.Write(file)
+	// 写文件流给Server, 会产生问题
+	//n, err := conn.Write(file)
+
+	// 使用小端编码, 使得size在前面, 让接收端知道要用多少bytes
+	binary.Write(conn, binary.LittleEndian, int64(size))
+
+	// copy文件为stream, 这里使用CopyN可以确认一定只传多少字节的数据, 从而给出一个EOF到最后
+	n, err := io.CopyN(conn, bytes.NewReader(file), int64(size))
 	if err != nil {
 		return err
 	}
@@ -73,7 +102,7 @@ func main() {
 
 	go func() {
 		time.Sleep(4 * time.Second)
-		sendFile(1000)
+		sendFile(4000) // 如果这里传入的bytes超出了server限制的大小, 会导致丢失, 所以最佳实践应该将file转换为流而不是直接传一个bytes数组
 	}()
 
 	server := &FileServer{}
