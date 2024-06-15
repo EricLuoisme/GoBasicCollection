2c5c4ecb5d4baf917d273a43f37c5458ddd642fa
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-06-15 12:27:14 +0800

    chore: edit unit tests

--- distribute/storage/store_test.go
+++ distribute/storage/store_test.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"testing"
 )
 
 func TestPathTransformFunc(t *testing.T) {
+	// call
 	key := "anotherPic"
 	pathName := CASPathTransformFunc(key)
-	fmt.Println(pathName)
+	targetPathName := "0f9f2/84da2/864d3/3a2be/14b51/0f6f0/fd538/3991d"
+	// check
+	if pathName != targetPathName {
+		t.Errorf("have %s want %s", pathName, targetPathName)
+	}
 }
 
 // TestNewStore 直接进行简单的保存测试
 func TestNewStore(t *testing.T) {
+	// call
 	opts := StoreOpts{
 		PathTransferFunc: DefaultPathTransformFunc,
 	}
 	s := NewStore(opts)
-
 	data := bytes.NewReader([]byte("some jpg bytes"))
+	// check
 	if err := s.writeStream("specialPic", data); err != nil {
 		t.Error(err)
 	}
