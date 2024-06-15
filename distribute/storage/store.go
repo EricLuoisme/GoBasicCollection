e8e193ee95b610be9e76d9691c7025f54fbd9f15
EricLuoisme <35546172+EricLuoisme@users.noreply.github.com>
2024-06-15 13:01:59 +0800

    refactor: edit the return type -> to struct, combine the split hash & original hash

--- distribute/storage/store.go
+++ distribute/storage/store.go
@@ -14,7 +14,7 @@ import (
 // CASPathTransformFunc 将Key进行hash, 再对hashString进行分段
 // 作用可以看作是相较于直接使用Key作为存储路径, 使用Hash可以平均的分布
 // 文件到具体的存储位置, e.g. -> 0f9f2/84da2/864d3/3a2be/14b51
-func CASPathTransformFunc(key string) string {
+func CASPathTransformFunc(key string) PathKey {
 	// [20]byte -> []byte -> [:]
 	hash := sha1.Sum([]byte(key))
 	hashStr := hex.EncodeToString(hash[:])
@@ -27,17 +27,29 @@ func CASPathTransformFunc(key string) string {
 		from, to := i*blockSize, (i+1)*blockSize
 		path[i] = hashStr[from:to]
 	}
-	return strings.Join(path, "/")
+
+	return PathKey{
+		PathName: strings.Join(path, "/"),
+		Original: hashStr,
+	}
 }
 
-type PathTransferFunc func(string) string
+type PathTransferFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	Original string
+}
 
 type StoreOpts struct {
 	PathTransferFunc PathTransferFunc
 }
 
-var DefaultPathTransformFunc = func(key string) string {
-	return key
+var DefaultPathTransformFunc = func(key string) PathKey {
+	return PathKey{
+		PathName: key,
+		Original: key,
+	}
 }
 
 type Store struct {
@@ -53,7 +65,7 @@ func NewStore(opts StoreOpts) *Store {
 func (s *Store) writeStream(key string, r io.Reader) error {
 	// 获取Path名称并进行os级别创建
 	pathName := s.PathTransferFunc(key)
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	if err := os.MkdirAll(pathName.PathName, os.ModePerm); err != nil {
 		return err
 	}
 	// 添加Buf进行读写
@@ -62,7 +74,7 @@ func (s *Store) writeStream(key string, r io.Reader) error {
 	// 名称也进行hash -> 使用md5
 	filenameBs := md5.Sum(buf.Bytes())
 	filename := hex.EncodeToString(filenameBs[:])
-	fullPath := pathName + "/" + filename
+	fullPath := pathName.PathName + "/" + filename
 	// 打开/创建文件, 路径默认为当前pkg路径下
 	f, err := os.Create(fullPath)
 	if err != nil {
--- distribute/storage/store_test.go
+++ distribute/storage/store_test.go
@@ -8,11 +8,13 @@ import (
 func TestPathTransformFunc(t *testing.T) {
 	// call
 	key := "anotherPic"
-	pathName := CASPathTransformFunc(key)
-	targetPathName := "0f9f2/84da2/864d3/3a2be/14b51/0f6f0/fd538/3991d"
+	pathKey := CASPathTransformFunc(key)
+	targetPathOriginal := "0f9f284da2864d33a2be14b510f6f0fd5383991d"
+	targetPathName := "0f9f284d/a2864d33/a2be14b5/10f6f0fd/5383991d"
 	// check
-	if pathName != targetPathName {
-		t.Errorf("have %s want %s", pathName, targetPathName)
+	if pathKey.PathName != targetPathName || pathKey.Original != targetPathOriginal {
+		t.Errorf("have %s want %s", pathKey.PathName, targetPathName)
+		t.Errorf("have %s want %s", pathKey.Original, targetPathOriginal)
 	}
 }
 
